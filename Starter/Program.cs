1095c4ae1ca11bf723af37ebb8bbb3e09cd3839b
Ramses Sanchez <ramsessanchez25@gmail.com>
2025-02-20 12:29:29 -0500

    Readded sophia, was not supposed to delete

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -1,22 +1,32 @@
-﻿using System;
-
-// initialize variables - graded assignments 
+﻿// initialize variables - graded assignments 
 int currentAssignments = 5;
-int [] sophiaScores = new int[] {90, 86, 87, 98, 100};
 
-int [] andrewScores = new int [] {92, 89, 81, 96, 90};
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-int [] emmaScores = new int [] {90, 85, 87, 98, 68};
 
-int [] loganScores = new int [] {90, 95, 87, 88, 96};
+
+
+
+int sophiaSum = 0;    // readded this, wasnt supposed to be deleted 
+
+decimal sophiaScore;   //must stay 
+
+sophiaScore = (decimal)sophiaSum / currentAssignments; // keep for now to use in foreach statements 
 
 
 
 Console.WriteLine("Student\t\tGrade\n");
+
+
+
 Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+
+
+
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
