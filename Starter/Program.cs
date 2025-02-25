1be54a62a66fc4b16140fb7ca2663b224a0aeb1c
Ramses Sanchez <ramsessanchez25@gmail.com>
2025-02-24 22:19:38 -0500

    Unit 5 if elseif else staments added

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -8,29 +8,35 @@ int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };    //Student Names
 
-int [] studentScores = new int [10];
+int[] studentScores = new int[10];
 
+string currentStudentLetterGrade = " ";
 
+Console.WriteLine("Student\t\tGrade\n");
 
 foreach (string name in studentNames)
 {
     string currentStudent = name;
+
+
     if (currentStudent == "Sophia")
-      studentScores = sophiaScores;
+        studentScores = sophiaScores;
 
-      else if (currentStudent == "Andrew")
-      studentScores = andrewScores;
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
 
-      else if (currentStudent == "Emma")
-      studentScores = emmaScores;
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
 
-      else if (currentStudent == "Logan")
-      studentScores = loganScores;
-    
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
 
     int sumAssignmentScore = 0;    // readded this, wasnt supposed to be deleted 
 
     decimal currentStudentGrade = 0;   //must stay 
+
+    
 
     foreach (int score in studentScores)
     {
@@ -40,9 +46,44 @@ foreach (string name in studentNames)
 
     currentStudentGrade = (decimal)(sumAssignmentScore) / currentAssignments; // keep for now to use in foreach statements 
 
+    if(currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
 
-    Console.WriteLine("Student\t\tGrade\n");
+        else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
 
+        else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+        else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+        else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+        else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+        else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+        else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+        else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+        else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+        else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+        else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+        else 
+        currentStudentLetterGrade = "F";
 
     Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
 }
