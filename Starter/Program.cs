6f3428227c19c6ae30ab3cffef8e2bddba2a202b
Ramses Sanchez <ramsessanchez25@gmail.com>
2025-02-24 21:25:40 -0500

    Outer names loop

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -6,31 +6,37 @@ int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-string [] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan"};    //Student Names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };    //Student Names
 
-foreach (string name in studentNames){
-    Console.WriteLine($"{name}");
+foreach (string name in studentNames)
+{
+    if (name == "Sophia")
+    {
+
+    int sophiaSum = 0;    // readded this, wasnt supposed to be deleted 
+
+    decimal sophiaScore;   //must stay 
+
+    foreach (int score in sophiaScores)
+    {
+        //We will add the exam scores
+        sophiaSum += score;
+    }
+
+    sophiaScore = (decimal)sophiaSum / currentAssignments; // keep for now to use in foreach statements 
+
+
+    Console.WriteLine("Student\t\tGrade\n");
+
+    Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+    }
+
 }
 
-int sophiaSum = 0;    // readded this, wasnt supposed to be deleted 
-
-decimal sophiaScore;   //must stay 
-
-foreach(int score in sophiaScores){
-                    //We will add the exam scores
-                    sophiaSum += score;
-}
 
 
 
-sophiaScore = (decimal)sophiaSum / currentAssignments; // keep for now to use in foreach statements 
 
-                
-Console.WriteLine("Student\t\tGrade\n");
-
-
-
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
 
 
 Console.WriteLine("Press the Enter key to continue");
